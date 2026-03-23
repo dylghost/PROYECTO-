10e234873c85b6fdca9e4b08833b39c2bc436943
sebasghost <dylancito.cabrera15@gmail.com>
2026-03-22 21:45:13 -0500

    Realizar el menu con las opciones que va a mostrar el programa al usuario

--- proyecto.cpp
+++ proyecto.cpp
@@ -1,5 +1,21 @@
-#include<iostream>
+
+/*
+AUTOR: Dylan Sebaastián Cabrera Enríquez
+FECHA: 22-03-2025
+TEMA: SISTEMA DE CONTROL DE INVENTARIO
+*/#include<iostream>
 using namespace std;
+void menu();
+ 
+void menu(){
+    cout << "\n===== INVENTARIO =====\n";
+    cout << "1. Registrar producto\n";
+    cout << "2. Mostrar inventario\n";
+    cout << "3. Valor total\n";
+    cout << "4. Buscar producto (MEJORA)\n";
+    cout << "5. Salir\n";
+    cout << "Seleccione: ";
+}
 int main(){
-    
+
 }
